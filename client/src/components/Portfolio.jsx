27a97a10b3bd120f5d4cf8a8b4c93ff5f4fe78bc
Prashantranamagar <ranamagar.prashant@gmail.com>
2025-09-05 13:49:59 +0545

    activate link

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -4,51 +4,50 @@ import ecommerce from "../assets/portfolio/ecommerce.png";
 import newssite from "../assets/portfolio/NewsSite.png";
 import ams from "../assets/portfolio/ams.jpeg";
 
-
 const Portfolio = () => {
   const portfolio = [
     {
       id: 1,
-      src: ems,
+      src: newssite,
+      demo: "https://prashantranamagar.pythonanywhere.com",
+      code: "https://github.com/username/newssite",
     },
     {
       id: 2,
-      src: ecommerce,
+      src: ems,
+      demo: "https://ems-demo.com",
+      code: "https://github.com/username/ems",
     },
     {
       id: 3,
-      src: ams,
+      src: ecommerce,
+      demo: "https://ecommerce-demo.com",
+      code: "https://github.com/username/ecommerce",
     },
     {
       id: 4,
-      src: newssite,
+      src: ams,
+      demo: "https://ams-demo.com",
+      code: "https://github.com/username/ams",
     },
-    // {
-    //   id: 5,
-    //   src: installNode,
-    // },
-    // {
-    //   id: 6,
-    //   src: reactWeather,
-    // },
+    
   ];
 
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full
-                 text-white md:h-screen  "
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
-          <p className="py-6 ">Check out some of my work here</p>
+          <p className="py-6">Check out some of my work here</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolio.map(({ id, src }) => (
+          {portfolio.map(({ id, src, demo, code }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -56,10 +55,16 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  onClick={() => window.open(demo, "_blank")}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  onClick={() => window.open(code, "_blank")}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   Code
                 </button>
               </div>
